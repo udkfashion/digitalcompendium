8cbc3bb212bb9070742dfe91a318f13cd06163e1
NielsGercama <nielsgercama@gmail.com>
2025-05-06 11:44:09 +0200

    fix map.js nielsgercama

--- nielsgercama/map.js
+++ nielsgercama/map.js
@@ -1,20 +1,20 @@
 const PAGES = {
-    4 : {
+    5 : {
         type : "youtube",
         link:"https://www.youtube.com/watch?v=Bx3O0N2nXcs&t=3s",
         posterlink:""
     },
-    6 : {
+    7 : {
         type:"video", //video (.mp4), miro (url), 3d (.glb)
         link:"media/examplevideo/video.mp4",
         posterlink:"media/examplevideo/poster.jpg",
     },
-	8 : {
+	9 : {
         type:"miro", //video, miro, 3d
         link:"https://miro.com/app/board/uXjVKE-Zstg=/",
         posterlink:""
 	},
-    10 : {
+    11 : {
         type:"3d", //video, miro, 3d
         link:"media/example3d/model.glb",
         posterlink:"media/example3d/poster.jpg"
@@ -23,7 +23,7 @@ const PAGES = {
 
 const AUDIO = {
     "media/exampleaudio/audio2.mp3" : {
-        "start" : 6,
+        "start" : 7,
         "end" : 9
     }
 }
